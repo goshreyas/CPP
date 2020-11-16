ca933897c406b63c5665010557ab0cbc2fa5215d
Shreyas <shreyas.ranjan@gmail.com>
2020-11-16 17:47:47 +0530

    Update CPP_tricks_Tips_v1.c

--- CPP_tricks_Tips_v1.c
+++ CPP_tricks_Tips_v1.c
@@ -4,5 +4,27 @@ Find max of k num:
     m = max({n1, n2, n3, ... nk});
     Ref: 
 
+---------------------------------------------------------------------------------------------------
 
 auto it = find()
+    
+---------------------------------------------------------------------------------------------------
+    
+Fast I/O for Competitive Programming
+1: https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
+2: https://leetcode.com/problems/range-sum-of-bst/submissions/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
+
+---------------------------------------------------------------------------------------------------
+
+
+---------------------------------------------------------------------------------------------------
