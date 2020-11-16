22cec3554365c5f203479d6d1492f9743e4f0054
Shreyas <shreyas.ranjan@gmail.com>
2020-11-16 17:49:49 +0530

    Update CPP_tricks_Tips_v1.c

--- CPP_tricks_Tips_v1.c
+++ CPP_tricks_Tips_v1.c
@@ -1,15 +1,15 @@
-
-
+---------------------------------------------------------------------------------------------------
+Tips#1:
 Find max of k num:
     m = max({n1, n2, n3, ... nk});
     Ref: 
 
 ---------------------------------------------------------------------------------------------------
-
+Tips#2:
 auto it = find()
     
 ---------------------------------------------------------------------------------------------------
-    
+Tips#3:
 Fast I/O for Competitive Programming
 1: https://www.geeksforgeeks.org/fast-io-for-competitive-programming/
 2: https://leetcode.com/problems/range-sum-of-bst/submissions/
