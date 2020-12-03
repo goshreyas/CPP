95897a163f2abd3350c2d34546c3742e86151996
Shreyas <shreyas.ranjan@gmail.com>
2020-12-03 10:50:43 +0530

    Update 5_mutex_try_lock.cpp

--- SystemProgramming/5_mutex_try_lock.cpp
+++ SystemProgramming/5_mutex_try_lock.cpp
@@ -1,17 +1,49 @@
 
 
 
+
 /*
-Ref: https://www.youtube.com/watch?v=UgcyOBUaR6M&list=PLk6CEY9XxSIAeK-EAh3hB4fgNvYkYmghp&index=5
+Syntax:
+
+#include <mutex>
+mutex m;
+//m.lock()    //<----------------  Blocking Call 
+m.try_lock()    //<----------------  Non Blocking Call: Return from this if unable to lock
+----  <Code Block>   -----
+m.unlock()
 
 */
+
 
 
 
 //======================================<Code Block(S)>======================================
 
 
+#include<bits/stdc++.h>
+//#include<thread>
+//#include<mutex>
 
+using namespace std;
+
+int myAmount = 0;
+mutex m;
+
+void addMoney() {
+	//m.lock();
+  m.try_lock();
+	++myAmount;
+	m.unlock();
+}
+
+int main() {
+	thread t1(addMoney);
+	thread t2(addMoney);
+	t1.join();
+	t2.join();
+	cout << myAmount << endl;
+	return 0;
+}
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++
 /*
@@ -21,8 +53,14 @@ Compile & Test:
 #> ./<binary:a.out/mutex.out>
 */
 
+
 //======================================<Code Block(E)>======================================
 
+
+/*
+Ref: https://www.youtube.com/watch?v=UgcyOBUaR6M&list=PLk6CEY9XxSIAeK-EAh3hB4fgNvYkYmghp&index=5
+
+*/
 
 
 
