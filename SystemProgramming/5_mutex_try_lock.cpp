d70c3744ea9961fbdedcd3b624aa3b2cd88238fa
Shreyas <shreyas.ranjan@gmail.com>
2020-12-03 10:45:41 +0530

    Update 5_mutex_try_lock.cpp

--- SystemProgramming/5_mutex_try_lock.cpp
+++ SystemProgramming/5_mutex_try_lock.cpp
@@ -13,4 +13,18 @@ Ref: https://www.youtube.com/watch?v=UgcyOBUaR6M&list=PLk6CEY9XxSIAeK-EAh3hB4fgN
 
 
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+/*
+Compile & Test:
+#> g++ -std=c++11 -pthread -g mutex.cpp
+#> g++ -std=c++11 -pthread -g mutex.cpp -o mutex.out
+#> ./<binary:a.out/mutex.out>
+*/
+
 //======================================<Code Block(E)>======================================
+
+
+
+
+
+
