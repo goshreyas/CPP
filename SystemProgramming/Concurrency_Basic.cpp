efc715cb36e661f5015a832d0fd331138acab2bb
Shreyas <shreyas.ranjan@gmail.com>
2020-12-06 03:36:30 +0530

    Update Concurrency_Basic.cpp

--- SystemProgramming/Concurrency_Basic.cpp
+++ SystemProgramming/Concurrency_Basic.cpp
@@ -2,6 +2,8 @@
 
 Go For Sleep:
 std::this_thread::sleep_for(std::chrono::seconds(1));
+std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
 
 Get Thread ID:
 std::thread::id this_id = std::this_thread::get_id();
