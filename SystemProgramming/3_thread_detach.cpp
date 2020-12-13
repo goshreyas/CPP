8633666434344e8cdc8ce3d730c212e345b8c444
Shreyas <shreyas.ranjan@gmail.com>
2020-12-13 12:07:58 +0530

    Create 3_thread_detach.cpp

---
+++ SystemProgramming/3_thread_detach.cpp
@@ -0,0 +1,40 @@
+
+
+
+//======================================<Code Block(S)>======================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void run(int count) {
+	cout<< "\nMy ID: " << this_thread::get_id() << endl;
+	cout<< "\nGoing to Sleep for :" << count << " seconds";
+	this_thread::sleep_for(chrono::seconds(count));
+	cout<< "\nAfter Sleeping for :" << count << " seconds";
+}
+
+
+int main() {
+	thread t1(run, 1000);
+	cout << "main()" << endl;
+	t1.detach();
+	cout << "main() after" << endl;
+	return 0;
+}
+
+/*
+Output:
+srb-go3@slave-node:~/dev_space/CPP_concurrency$ g++ -std=c++11 -pthread -g 3_thread_detach.cpp 
+srb-go3@slave-node:~/dev_space/CPP_concurrency$ ./a.out 
+main()
+My ID: 140405963212544
+
+Going to Sleep for :10 seconds
+main() after
+srb-go3@slave-node:~/dev_space/CPP_concurrency$
+*/
+
+
+//======================================<Code Block(S)>======================================
+
+
