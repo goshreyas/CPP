f3a2b141d471db64ff0e32bcab2ce47c9b73cba2
Shreyas <shreyas.ranjan@gmail.com>
2020-12-18 03:26:34 +0530

    Create thread_id.cpp

---
+++ SystemProgramming/thread_id.cpp
@@ -0,0 +1,33 @@
+
+
+
+//======================================<Code Block(S)>======================================
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void thread_function(string s)
+{
+    std::cout << "thread function\n";
+}
+
+
+int main()
+{
+    std::string s = "Shreyas B ";
+    //std::thread t(&thread_function, std::move(s));  // Working
+    std::thread t(&thread_function, s);
+    std::cout << "main thread message = " << s << std::endl;
+
+    std::cout << "main thread id = " << std::this_thread::get_id() << std::endl;
+    std::cout << "child thread id = " << t.get_id() << std::endl;
+
+    t.join();
+    return 0;
+}
+
+//======================================<Code Block(E)>======================================
+
+
+
+
