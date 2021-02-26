8ded96b56ca61f14bd13c4fa1e95aea3c66f6288
Shreyas <shreyas.ranjan@gmail.com>
2021-02-26 11:54:19 +0530

    Update CPP_tricks_Tips_v1.c

--- CPP_tricks_Tips_v1.c
+++ CPP_tricks_Tips_v1.c
@@ -73,7 +73,7 @@ Ref: https://leetcode.com/problems/string-to-integer-atoi/
 Value of INT_MAX is +2147483647
 Value of INT_MIN is -2147483648
     
-int modulo = 1e10+7; // 10000000007  == 1𝑒10+7
+int modulo = 1e9+7; // 1000000007  == 1𝑒9+7
 
 ---------------------------------------------------------------------------------------------------
 
