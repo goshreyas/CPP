d7f4c9a0fba951545c7c60d4096ec6d1d2d51174
Shreyas <shreyas.ranjan@gmail.com>
2021-07-25 12:36:29 +0530

    Update semaphore_hello.c

--- SystemProgramming/semaphore_hello.c
+++ SystemProgramming/semaphore_hello.c
@@ -25,12 +25,15 @@ void* thread(void* arg)
 int main()
 {
 	sem_init(&mutex, 0, 1);              // NOTE: Only Single thread can hold semaphore
+	
 	pthread_t t1,t2;
 	pthread_create(&t1,NULL,thread,NULL);
 	//sleep(2);
 	pthread_create(&t2,NULL,thread,NULL);
-    pthread_join(t1,NULL);
+        
+	pthread_join(t1,NULL);
 	pthread_join(t2,NULL);
+	
 	sem_destroy(&mutex);
 	return 0;
 }
