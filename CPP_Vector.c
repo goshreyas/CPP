ce9bd045d4284000ee03516e376f85d119af9d98
Shreyas <shreyas.ranjan@gmail.com>
2022-04-27 11:28:47 +0530

    Create CPP_Vector.c

---
+++ CPP_Vector.c
@@ -0,0 +1,8 @@
+
+1. Vector of set
+vector<set<char>> row(9), col(9), cell(9); // Working
+unordered_set<int> row[9], col[9], cell[9];  // Working
+
+https://leetcode.com/problems/valid-sudoku/submissions/
+
+
