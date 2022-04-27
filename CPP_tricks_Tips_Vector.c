3badf7684192a00a34eecb84df4b8acb81cd6b84
Shreyas <shreyas.ranjan@gmail.com>
2022-04-27 11:29:54 +0530

    Update CPP_tricks_Tips_Vector.c

--- CPP_tricks_Tips_Vector.c
+++ CPP_tricks_Tips_Vector.c
@@ -4,5 +4,5 @@ vector<set<char>> row(9), col(9), cell(9); // Working
 unordered_set<int> row[9], col[9], cell[9];  // Working
 
 https://leetcode.com/problems/valid-sudoku/submissions/
-
+https://leetcode.com/problems/sudoku-solver/submissions/
 
