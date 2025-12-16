ae0093a56dd066eca0e42bd3bf6867042d3ed6e1
Shreyas <shreyas.ranjan@gmail.com>
2025-12-16 15:12:01 +0530

    Update PrintOddEven.cpp

--- SystemProgramming/PrintOddEven.cpp
+++ SystemProgramming/PrintOddEven.cpp
@@ -53,3 +53,57 @@ printEven() Num: 8
 printOdd() Num: 9
 printEven() Num: 10
 srb-go3@slave-node:~/system_programming$ 
+
+
+=====================================================================================================================
+#include<bits/stdc++.h>
+using namespace std;
+
+mutex mtx;
+condition_variable cv;
+bool turn;
+int nums;
+
+void printOdd() {
+  unique_lock<mutex> lock(mtx);
+  while(nums > 0) {
+    //printf("\n Conditional Lock in printOdd()");
+    cv.wait(lock, [&](){return (turn == true);}); // Turn: True - print odd number
+    printf("\nprintOdd: %d", nums);
+    nums--;
+    turn = false;
+    cv.notify_all();
+  }
+}
+
+void printEven() {
+  unique_lock<mutex> lock(mtx);
+  while(nums > 0) {
+    //printf("\n Conditional Lock in printEven()");
+    cv.wait(lock, [&](){return (turn == false);}); // Turn: False - print even number
+    printf("\nprintEven: %d", nums);
+    nums--;
+    turn = true;
+    cv.notify_all();
+  }
+}
+
+
+int main(void) {
+    printf("\nHello Mutex Testing Started .... ");
+    turn = true;
+    //int num = 11;
+    nums = 11;
+    //thread t1(printOdd, num);
+    thread t1(printOdd);
+    //thread t2(printEven, num);
+    thread t2(printEven);
+    t1.join();
+    t2.join();
+    printf("\nHello Mutex Testing STOPPed .... ");
+    return 0;
+}
+	
+
+
+	
